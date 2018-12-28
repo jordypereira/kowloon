a60e8bf0fd154dcf343aab44e30d1bee0fee2b41
Jordy Pereira <jordy.pereira@hotmail.com>
2018-12-28 20:38:32 +0100

    Added categories

---
+++ components/Categories.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div v-for="item in categoryList" :key="item.id">
+      <CategoryItem :category="item"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import CategoryItem from '@/components/CategoryItem'
+
+export default {
+  components: {
+      CategoryItem,
+  },
+  data() {
+    return {
+      categoryList: [
+        {
+          id: 1,
+          name: 'Dogs',
+          url: dogsSymbol,
+          color: 'pink',
+        },
+        {
+          id: 2,
+          name: 'Cats',
+          url: catsSymbol,
+          color: 'indigo',
+        },
+        {
+          id: 3,
+          name: 'Fish',
+          url: fishSymbol,
+          color: 'yellow',
+        },
+        {
+          id: 4,
+          name: 'Birds',
+          url: birdsSymbol,
+          color: 'green-light',
+        },
+        {
+          id: 5,
+          name: 'Small Animals',
+          url: smallAnimalsSymbol,
+          color: 'blue-light',
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ components/CategoryItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col justify-between">
+    <img :src="category.url" :alt="`Symbol of the category ${category.name}`">
+    <p class="section-title">{{ category.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    category: Object,
+  }
+}
+</script>
