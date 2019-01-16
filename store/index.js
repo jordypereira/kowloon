466da0b4fd17f9de74b9a2b3be97f11609020a9d
Jordy Pereira <jordy.pereira@hotmail.com>
2019-01-16 23:08:42 +0100

    Added a store

---
+++ store/index.js
@@ -0,0 +1,66 @@
+export const state = () => ({
+  categories: {
+    dog: {
+      name: 'Dogs',
+      shortName: 'Dog',
+      color: 'pink',
+    },
+    cat: {
+      name: 'Cats',
+      shortName: 'Cat',
+      color: 'indigo',
+    },
+    fish: {
+      name: 'Fishes',
+      shortName: 'Fish',
+      color: 'yellow',
+    },
+    bird: {
+      name: 'Birds',
+      shortName: 'Bird',
+      color: 'green-light',
+    },
+    smallAnimal: {
+      name: 'Small Animals',
+      shortName: 'Small Animal',
+      color: 'blue-light',
+    },
+    other: {
+      name: 'Other',
+      color: 'beige',
+    },
+  }
+})
+
+export const getters = {
+  getCategory: state => name => {
+    switch (name) {
+    case 'dog':
+    case 'Dog':
+    case 'dogs':
+    case 'Dogs':
+      return state.categories.dog
+    case 'cat':
+    case 'Cat':
+    case 'cats':
+    case 'Cats':
+      return state.categories.cat
+    case 'fish':
+    case 'Fish':
+      return state.categories.fish
+    case 'bird':
+    case 'Bird':
+    case 'birds':
+    case 'Birds':
+      return state.categories.bird
+    case 'small-animals':
+    case 'small animals':
+    case 'Small-Animals':
+    case 'Small Animals':
+      return state.categories.smallAnimal
+      
+    default: 
+      return state.categories.other
+    }
+  }
+}
