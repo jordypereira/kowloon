67b51f037a679780b4cafcc89c0aadba7cbf3e34
Jordy Pereira <jordy.pereira@hotmail.com>
2019-01-17 22:03:54 +0100

    Newsletter Netlify

--- components/NewsletterBanner.vue
+++ components/NewsletterBanner.vue
@@ -11,19 +11,82 @@
       <div>
         <div class="sub-title">Subscribe to our newsletter</div>
         <div class="text-grey-400er mb-5">Lorum ipsum dolor sit amet</div>
-        <div class="flex">
-          <label aria-label="Enter your email for the newsletter">
-            <input type="text" placeholder="Domain @ name.com" class="input-text">
+        <form
+          class="flex"
+          name="newsletter"
+          method="POST"
+          data-netlify="true"
+          @submit.prevent="submitNewsletterForm"
+        >
+          <label aria-label="Enter your email for the newsletter" class="flex flex-col">
+            <input
+              type="text"
+              placeholder="Domain @ name.com"
+              class="input-text"
+              v-model="form.email"
+            >
+            <span class="text-pink">{{ formErrors.email }}</span>
           </label>
           <button class="btn-input" aria-label="Signs you up for the news letter.">OK</button>
-        </div>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'NewsletterBanner',
+  data() {
+    return {
+      form: {
+        email: '',
+      },
+      formErrors: {
+        email: '',
+      },
+    }
+  },
+  methods: {
+    validateEmail(email) {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    },
+    clearErrors() {
+      this.formErrors = {
+        email: '',
+        message: '',
+      }
+    },
+    submitNewsletterForm() {
+      const axiosConfig = {
+        header: { "Content-Type": "application/x-www-form-urlencoded" }
+      };
+      // Validate
+      this.clearErrors()
+      if(!this.validateEmail(this.form.email)) {
+        this.formErrors.email = 'Please enter a correct e-mail address.'
+        return
+      }
+
+      axios.post(
+        "/",
+        this.encode({
+          "form-name": "newsletter",
+          ...this.form
+        }),
+        axiosConfig
+      );
+    },
+    encode (data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    },
+  }
 }
 </script>
