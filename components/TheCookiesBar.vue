f86c328ddb613dfe9b566cd70dd772ff33943d56
Jordy Pereira <jordy.pereira@hotmail.com>
2019-01-18 20:18:48 +0100

    Added cookies popup

---
+++ components/TheCookiesBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bg-beige w-full absolute pin-t md:relative z-30 shadow-lg">
+    <i
+      class="absolute pin-t pin-r icon icon-x-close icon-orange m-4 md:m-5 hover:cursor-pointer"
+      @click="setAcceptedCookie(false)"
+    ></i>
+    <div class="flex justify-center pt-27px md:pt-12 pb-2 md:pb-10">
+      <div class="hidden md:flex relative ml-2" style="height: 123px;">
+        <i class="icon icon-cookie"></i>
+        <i class="icon icon-crumble absolute pin-b pin-r mr-10 mb-6"></i>
+      </div>
+      <div class="md:w-2/3 mx-7px md:ml-16">
+        <h2 class="text-cookie-title text-orange relative inline">
+          Cookies
+          <i class="md:hidden icon icon-crumble absolute pin-t pin-r -mr-12 -mt-2"></i>
+        </h2>
+        <p
+          class="mt-4 text-grey-900"
+          style="max-width: 688px;"
+        >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Duis voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <button class="btn btn-md bg-orange mt-6" @click="setAcceptedCookie(true)">ok, verder surfen</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheCookiesBar',
+  methods: {
+    setAcceptedCookie(value) {
+      localStorage.setItem('acceptedCookies', value)
+      this.$emit('acceptedCookies')
+    },
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="flex bg-black">
-    <TheNavBar/>
-    <div class="bg-grey-900 w-full min-h-screen">
-      <nuxt/>
+  <div>
+    <TheCookiesBar v-if="!acceptedCookies" @acceptedCookies="acceptedCookies = true"/>
+    <div class="flex bg-black">
+      <TheNavBar/>
+      <div class="bg-grey-900 w-full min-h-screen">
+        <nuxt/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import TheNavBar from '@/components/TheNavBar'
+import TheCookiesBar from '@/components/TheCookiesBar'
 
 export default {
   components: {
     TheNavBar,
+    TheCookiesBar,
   },
-  
+  data() {
+    return {
+      acceptedCookies: false,
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('acceptedCookies')) {
+      this.acceptedCookies = true
+    }
+  }
 }
 </script>
